2b934d92fc2e81da6b125947602e9514168bcc5f
Caio Belfort <caiobelfort90@gmail.com>
2019-06-26 15:13:40 -0300

    Movendo mapp_plugin.py para a pasta principal

--- mapp_plugin.py
+++ mapp_plugin.py
@@ -3,5 +3,7 @@ from airflow.plugins_manager import AirflowPlugin
 from mapp.operators.dv_operators import HubFormatterOperator, LinkFormatterOperator, DatavaultInsertOperator
 
 class MappPlugin(AirflowPlugin):
-    name = "mapp_airflow_extensions"
-    operators = [HubFormatterOperator, LinkFormatterOperator, DatavaultInsertOperator]
+    name = "mapp"
+    operators = [HubFormatterOperator,
+                 LinkFormatterOperator,
+                 DatavaultInsertOperator]
