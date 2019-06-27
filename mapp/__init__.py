e2ed0aebca7afa48bfb352709a91b9efeb2a933a
Caio Belfort <caiobelfort90@gmail.com>
2019-06-27 18:19:29 -0300

    SatteliteFormatter

--- mapp/__init__.py
+++ mapp/__init__.py
@@ -5,7 +5,7 @@ from mapp.operators.mssql_operators import *
 from mapp.operators.gcs_operators import *
 
 class MappPlugin(AirflowPlugin):
-    name = "mapp.dv"
+    name = "mapp"
     operators = [
         HubFormatterOperator,
         LinkFormatterOperator,
